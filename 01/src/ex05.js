2954ff1185e2a486b98325118a8b2cbb4396f59b
yooss2006 <yooss2006@naver.com>
2023-07-14 12:54:29 +0900

    Feat: 애니메이션 기초 기능 구현

---
+++ 01/src/ex05.js
@@ -0,0 +1,61 @@
+import * as THREE from "three";
+
+export default function example() {
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.x = 1;
+  camera.position.y = 2;
+  camera.position.z = 5;
+  scene.add(camera);
+
+  //매개변수 1. 색상, 2. 빛의 강도
+  const light = new THREE.DirectionalLight(0xffffff, 1); //태양빛과 비슷하다. 무대의 요소들에 전체적으로 다 조명을 준다.
+  light.position.x = 2;
+  light.position.z = 2;
+  scene.add(light);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({
+    color: "#ff0000",
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  scene.add(mesh);
+
+  function draw() {
+    // mesh.rotation.y += 0.1;
+    mesh.rotation.y += THREE.MathUtils.degToRad(1);
+    mesh.position.y += 0.01;
+    if (mesh.position.y > 3) {
+      mesh.position.y = 0;
+    }
+
+    renderer.render(scene, camera);
+
+    // window.requestAnimationFrame(draw);
+    renderer.setAnimationLoop(draw);
+  }
+  draw();
+
+  const setSize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    draw();
+  };
+  window.addEventListener("resize", setSize);
+}
